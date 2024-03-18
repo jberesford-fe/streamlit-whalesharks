dc26074db631640a355eb837b14c51395fa017da
b-justin <justin.beresford@deliveroo.co.uk>
2024-03-18 09:32:18 +0000

    first dashboard

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
-print(
-    "this line is toooooooooooooooooooooooooooooooooooooooooooooooooooooo long"
-)
+import streamlit as st
+
+
+def main():
+    st.title("Car Data Dashboard")
+
+    st.write("This is a test dashboard for whalesharks")
+
+
+if __name__ == "__main__":
+    main()
