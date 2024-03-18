5003ce1cfae6abf615cb5fce947b49981a85e111
b-justin <justin.beresford@deliveroo.co.uk>
2024-03-18 10:26:31 +0000

    protect app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 import streamlit as st
+from utils import check_password
 
 
 def main():
+
+    if not check_password():
+        st.stop()  # Do not continue if check_password is not True.
+
     st.title("Whalesharks Dashboard")
 
     st.write("This is a test dashboard for whalesharks")
